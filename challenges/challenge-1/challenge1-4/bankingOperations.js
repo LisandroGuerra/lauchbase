5e6d87229e768c30ad18b84150b7e7107325b836
LisandroGuerra <lisandro.digital@gmail.com>
2020-06-24 08:19:35 -0300

    challenge 1-4 structure

---
+++ challenges/challenge-1/challenge1-4/bankingOperations.js
@@ -0,0 +1,33 @@
+const user = {
+    name: "Mariana",
+    transactions: [],
+    balance: 0
+  };
+
+function createTransaction(transaction){
+    // Exemple:
+    // {
+    //     type: 'credit',
+    //     value: 50.5
+    // }
+    // {
+    //     type: 'debit',
+    //     value: 30.4
+    // }
+
+    //Relatórios
+    function getHigherTransactionByType(transactionType){
+        //getHigherTransactionByType("credit"); // { type: 'credit', value: 120 }
+
+    }
+
+    function getAverageTransactionValue(){
+        //getAverageTransactionValue(); // 83.3
+        
+    }
+
+    function getTransactionCount(){
+        //getTransactionsCount(); // { credit: 5, debit: 3 }
+        
+    }
+}
